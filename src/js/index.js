8f70b442c405476c3d54acc02f788ba14e61f259
ishidaaoi <aoi_aoi_0123@icloud.com>
2021-12-07 18:12:46 +0900

    実装_04

--- src/js/index.js
+++ src/js/index.js
@@ -56,3 +56,44 @@ $(() => {
     $(".js-topHeroList li:first-child").appendTo(".js-topHeroList");
   }, 4000);
 });
+
+
+// $(() => {
+// // eslint-disable-next-line no-undef
+// const bar = new ProgressBar.Line(loading_text, {//id名を指定
+//     strokeWidth: 0,//進捗ゲージの太さ
+//     duration: 1000,//時間指定(1000＝1秒)
+//     trailWidth: 0,//線の太さ
+//     text: {//テキストの形状を直接指定
+//         style: {
+//             position:'absolute',
+//             left:'50%',
+//             top:'50%',
+//             margin:'0',
+//             transform:'translate(-50%,-50%)',
+//             'font-family':'sans-serif',
+//             'font-size':'1.5rem',
+//             color:'#fff',
+//         },
+//         autoStyleContainer: false //自動付与のスタイルを切る
+//     },
+//     step: function(state, bar) {
+//         bar.setText(`${Math.round(bar.value() * 100)  } %`); //テキストの数値
+//     }
+// });
+// //アニメーションスタート
+// bar.animate(1.0, () => {//バーを描画する割合を指定します 1.0 なら100%まで描画
+//     $("#loading").delay(500).fadeOut(800);//アニメーションが終わったら#loadingをフェードアウト
+// });
+// });
+
+
+$(() => {
+  $(".js-inner").hide(); //デフォルトではddを隠す
+   const $Button = $(".js-box");
+  $Button.on("click",() => {
+    $(".js-inner").slideToggle();
+  const $sign =  $(".js-inner");
+    $sign.toggleClass("open");
+  })
+});
